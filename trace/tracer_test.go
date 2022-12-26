73768c9f1d6982072c631b37365340721332b28f
lotadcate <lotadcate@gmail.com>
2022-12-26 18:24:51 +0900

    implement unittest

---
+++ trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnil")
+	} else {
+		tracer.Trace("Hello, trace package")
+		if buf.String() != "Hello, trace package\n" {
+			t.Errorf("'%s'という誤った文字列", buf.String())
+		}
+	}
+}
